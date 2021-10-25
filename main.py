470192cf29ed9b2638cae2f1f9765bcb6a10c561
HN plays <92354190+HNwastaken@users.noreply.github.com>
2021-10-25 13:39:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ block_1_3 = JumpBlock(position = (0, -20, 25))
 block_1_4 = NormalBlock(position = (0, 0, 32))
 block_1_5 = SpeedBlock(position = (0, 0, 42))
 
-endBlock = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA", position = (0, 0, 55))
+endBlock = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA", position = (0, 0, 90))
 
 Sky()
 
