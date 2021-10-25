2e630f7bc0858c0dfb734361ddbd2776dddb8250
HN plays <92354190+HNwastaken@users.noreply.github.com>
2021-10-25 13:41:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ player.SPEED = normalSpeed
 light = PointLight(parent = camera, color = color.white, position = (0, 10, -1.5))
 AmbientLight(color = color.rgba(100, 100, 100, 0.1))
 
-startBlock = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA")
+#stage 1
+StartStage1 = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA")
 
 block_1_1 = NormalBlock(position = (0, 1, 10))
 block_1_2 = NormalBlock(position = (0, 2, 17))
@@ -22,7 +23,7 @@ block_1_3 = JumpBlock(position = (0, -20, 25))
 block_1_4 = NormalBlock(position = (0, 0, 32))
 block_1_5 = SpeedBlock(position = (0, 0, 42))
 
-endBlock = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA", position = (0, 0, 90))
+EndStage1 = Entity(model = "cube", scale = (10, 1, 10), collider = "box", texture = "white_cube", color = "#CACACA", position = (0, 0, 90))
 
 Sky()
 
